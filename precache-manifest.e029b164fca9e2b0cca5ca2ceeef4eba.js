06974eeb2130c9398490b986f622f364b7a6b610
Himanshu <himanshu1988mishra@gmail.com>
2020-08-15 06:53:18 +0530

    Updates

---
+++ precache-manifest.e029b164fca9e2b0cca5ca2ceeef4eba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672d12e718a30b3b82a61e45fa018427",
+    "url": "/robo-squad/index.html"
+  },
+  {
+    "revision": "9acd7889afa6d7b4f768",
+    "url": "/robo-squad/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9fc3c248407c8ba4a3f3",
+    "url": "/robo-squad/static/css/main.79b8be6c.chunk.css"
+  },
+  {
+    "revision": "9acd7889afa6d7b4f768",
+    "url": "/robo-squad/static/js/2.1b3ba2f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robo-squad/static/js/2.1b3ba2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc3c248407c8ba4a3f3",
+    "url": "/robo-squad/static/js/main.ced41be6.chunk.js"
+  },
+  {
+    "revision": "970f9f88cf3234fc03c0",
+    "url": "/robo-squad/static/js/runtime-main.8ef48d43.js"
+  }
+]);
